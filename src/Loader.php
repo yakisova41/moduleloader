e544faade40b60b1380f940627ed3b4302d4a992
yakisova41 <yakisoba5emon@gmail.com>
2022-07-20 14:18:06 +0000

    2.01

--- src/Loader.php
+++ src/Loader.php
@@ -11,7 +11,7 @@ class Loader
 
         if(file_exists($modulePath))
         {
-            require_once($modulePath);
+            require($modulePath);
         }
         else
         {
---
+++ src/Module.php
@@ -0,0 +1,174 @@
+<?php
+namespace Yakisova41\ModuleLoader;
+
+class Module
+{
+    private static $modulePath;
+    private static $importMode;
+    private static $moduleName;
+    private static $modules = [];
+    private static $styles = "";
+    private static $classes = [];
+    
+    public static function import(
+        $modulePath,
+        $moduleName = false
+    )
+    {
+        self::ErrorHandle();
+
+        if(file_exists($modulePath.'.php'))
+        {
+            self::$modulePath = $modulePath;
+            self::$importMode = 'module';
+        }
+        else if(file_exists($modulePath.'.style.json'))
+        {
+            self::$modulePath = $modulePath;
+            self::$importMode = 'style';
+        }
+        else
+        {
+            trigger_error("File \"$modulePath\" not found", E_USER_WARNING);
+            return;
+        }
+
+        if(self::$importMode === 'module')
+        {
+            if($moduleName)
+            {
+                self::$moduleName = $moduleName;
+            }
+            else
+            {
+                self::$moduleName = 'default';
+            }
+
+            return self::moduleImport($modulePath.'.php');
+        }
+        else if(self::$importMode === 'style')
+        {
+            return self::styleImport($modulePath.'.style.json');
+        }
+    }
+
+    public static function exportDefault(
+        $callBack
+    )
+    {
+        self::$modules['default'] = $callBack;
+    }
+
+    public static function export(
+        $callBack,
+        $moduleName
+    )
+    {
+        self::$modules[$moduleName] = $callBack;
+    }
+
+    public static function style()
+    {
+        return "<style>".self::$styles."</style>";
+    }
+
+    private static function moduleImport(
+        $modulePath
+    )
+    {
+        if(file_exists($modulePath))
+        {
+            require($modulePath);
+
+            if(isset(self::$modules[self::$moduleName]))
+            {
+                return self::$modules[self::$moduleName];
+            }
+
+            trigger_error(self::$moduleName." module not found in imported file", E_USER_WARNING);
+            return;
+        }
+    }
+
+    private static function styleImport(
+        $stylePath
+    )
+    {
+        $styleArray = \json_decode(\file_get_contents($stylePath));
+        $styleStr = "";
+        $classes = [];
+        $last = "";
+
+        foreach($styleArray as $styleSelector => $stylePropatys)
+        {  
+            if($styleSelector === 'media-query')
+            {
+                $styleStr = $styleStr.$stylePropatys."{".$styleStr;
+                $last = $last.'}';
+            }
+
+            $hash = self::ramdom(15);
+            Module::$classes[$styleSelector] = $hash;
+            $styleSelector = $hash;
+
+            foreach($stylePropatys as $stylePropaty => $value)
+            {
+                $styles = "";
+                $styles = $styles.$stylePropaty.':'.$value.';';
+            }
+            $styleStr = $styleStr.'.'.$styleSelector."{".$styles."}";
+        }
+    
+        self::$styles = self::$styles.$styleStr.$last;
+
+        return function($className){
+            
+            return Module::$classes[$className];
+        };
+    }
+    
+    private static function ramdom($length)
+    {
+        $words = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+        ];
+
+        $hash = "";
+        $i = 0;
+
+        while($i < $length)
+        {
+            $hash = $hash.$words[array_rand($words)];
+            $i++;
+        }
+
+        return $hash;
+    }
+
+    private static function ErrorHandle()
+    {
+        set_error_handler(function($errno, $errstr){
+            if (!(error_reporting() & $errno))
+            {
+                return;
+            }
+
+            $errstr = htmlspecialchars($errstr);
+            $backtraces = debug_backtrace();
+            $lastbacktrace = $backtraces[array_key_last($backtraces)];
+
+            $errfile = $lastbacktrace['file'];
+            $errline = $lastbacktrace['line'];
+
+            switch ($errno)
+            {
+                case E_USER_WARNING:
+                    echo "<br /><b>ModuleLoader Warning</b>: $errstr in <b>$errfile</b> on line <b>$errline</b><br />\n";
+                    break;
+                case E_USER_ERROR:
+                    echo "<br /><b>ModuleLoader Error</b>: $errstr in <b>$errfile</b> on line <b>$errline</b><br />\n";
+                    break;
+            }
+        });
+    }
+}
